5b76b977350f98bbfb8e3f4581d727a3390a053b
superkevinigt <superkevingit@gmail.com>
2016-08-26 16:08:18 +0800

    　“＇＇

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -12,7 +12,7 @@
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
                 <li class="active"><a href="{{ url('user/info') }}">用户信息<span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Link</a></li>
+                <li><a href="#">{{ url() }}</li>
             @if (Auth::user())
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Oauth2<span class="caret"></span></a>
